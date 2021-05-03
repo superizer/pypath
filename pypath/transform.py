44e0be47c2c4f637563e76c739144d735217135f
superizer <superizer.tone@gmail.com>
2021-05-03 19:39:37 +0700

    feat: A geometric and color gradient distance transform
    This special distance transform prepares for watershed technique.

--- pypath/transform.py
+++ pypath/transform.py
@@ -82,3 +82,68 @@ def mopho_process(I_GRAY, kernels):
    cells_images = img_as_ubyte(filled_img)
 
    return cells_images
+
+def geometric_distance(filled_img):
+   '''
+   Calculate geometric distance from binary image.
+   The input image consists of white object on black background.
+   input: binary image
+   output: geometric distance
+   '''
+   return cv2.distanceTransform(filled_img, cv2.DIST_L2, 3)
+
+def geometric_and_color_gradient_distance(color_img, filled_img):
+   '''
+   The special distance that combine the geometric distance with the color gradient transform
+   from Mouelhi et al. 'A new automatic image analysis method for assessing estrogen receptors’ status in breast tissue specimens'
+   input: color image and binary image
+   output: special distance that merge geometric distance with color gradeint transform
+   '''
+   b_img = color_img[:,:,0].copy()
+   g_img = color_img[:,:,1].copy()
+   r_img = color_img[:,:,2].copy()
+
+   grad_x     = cv2.Sobel(r_img,cv2.CV_64F,1,0,ksize=5)
+   grad_y     = cv2.Sobel(r_img,cv2.CV_64F,0,1,ksize=5)
+   abs_grad_x = cv2.convertScaleAbs(grad_x)
+   abs_grad_y = cv2.convertScaleAbs(grad_y)
+   r_grad     = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+
+   grad_x     = cv2.Sobel(g_img,cv2.CV_64F,1,0,ksize=5)
+   grad_y     = cv2.Sobel(g_img,cv2.CV_64F,0,1,ksize=5)
+   abs_grad_x = cv2.convertScaleAbs(grad_x)
+   abs_grad_y = cv2.convertScaleAbs(grad_y)
+   g_grad     = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+
+   grad_x     = cv2.Sobel(b_img,cv2.CV_64F,1,0,ksize=5)
+   grad_y     = cv2.Sobel(b_img,cv2.CV_64F,0,1,ksize=5)
+   abs_grad_x = cv2.convertScaleAbs(grad_x)
+   abs_grad_y = cv2.convertScaleAbs(grad_y)
+   b_grad     = cv2.addWeighted(abs_grad_x, 0.5, abs_grad_y, 0.5, 0)
+
+   height, width = filled_img.shape
+
+   g = np.zeros((height,width,3), np.uint8)
+
+   g[:,:,0] = b_grad
+   g[:,:,1] = g_grad
+   g[:,:,2] = r_grad
+
+
+   g_min = np.min(g)
+   g_max = np.max(g)
+
+   g_and_cg_dist = np.zeros((height,width,3), np.uint8)
+   g_dist        = geometric_distance(filled_img)
+
+   for i in range(3):
+      g_and_cg_dist[:,:,i] = g_dist * np.exp(1 - (g[:,:,i] - g_min)/(g_max - g_min))
+
+   sigma        = 15
+   max_d        = np.max(g_and_cg_dist)
+   g_and_cg_dist = max_d - g_and_cg_dist 
+   g_and_cg_dist = cv2.GaussianBlur(g_and_cg_dist,(sigma,sigma),0)
+
+   cv2.normalize(g_and_cg_dist, g_and_cg_dist, 0, 1, cv2.NORM_MINMAX)
+
+   return g_and_cg_dist
