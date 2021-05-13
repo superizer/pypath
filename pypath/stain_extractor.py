c0b65e72e7c69d98006c9659428ed4e0d4b178f7
superizer <superizer.tone@gmail.com>
2021-05-13 11:23:50 +0700

    fix: IHC color sorting
    Previously, IHC extractors used pink color for sorting. It should be blue color.

--- pypath/stain_extractor.py
+++ pypath/stain_extractor.py
@@ -41,8 +41,25 @@ def IHC_color_decon_Vahadane(I_RGB):
    output: I_DAB, I_H
    '''
    color_img = staintools.LuminosityStandardizer.standardize(I_RGB)
-   h, dab    = HE_color_decon_Vahadane(color_img)
-   return dab, h
+   OD        = convert_RGB_to_OD(color_img).reshape((-1, 3))
+
+
+   param = { 'K' : 2, 'lambda1' : 0.1 }
+   D     = spams.trainDL(X=OD.T, **param)
+
+   # Sort h before dab by blue color checking
+   if D[2, 1] < D[2, 0]:
+      D = D[:,[1,0]]
+
+   param  = { 'pos' : True, 'lambda1' : 0.1 }
+   Hs_vec = spams.lasso(X=OD.T, D=D, **param).T
+
+   vdAs  = Hs_vec[:,0]*D[:,0].reshape((1,3))
+   h     = convert_OD_to_RGB(vdAs).reshape(I_RGB.shape)
+
+   vdAs  = Hs_vec[:,1]*D[:,1].reshape((1,3))
+   dab   = convert_OD_to_RGB(vdAs).reshape(I_RGB.shape)
+   return h, dab
 
 def IHC_color_decon_Mouelhi(I_RGB):
    '''
@@ -63,8 +80,8 @@ def IHC_color_decon_Mouelhi(I_RGB):
    D[2,1] = 0.4475
    D = np.asfortranarray(D, dtype=np.float64)
 
-   # # Sort I_H before I_E by pink color checking
-   if np.abs(D[0, 0] - D[2, 0]) < np.abs(D[0, 1] - D[2, 1]): 
+   # Sort h before dab by blue color checking
+   if D[2, 1] < D[2, 0]:
       D = D[:,[1,0]]
 
 
